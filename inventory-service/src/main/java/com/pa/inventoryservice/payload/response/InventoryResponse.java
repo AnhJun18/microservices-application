cea1c410f6416f7d00db39ff31b7d9cef349836f
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2023-05-04 16:53:17 +0700

    check inventory from order service

---
+++ inventory-service/src/main/java/com/pa/inventoryservice/payload/response/InventoryResponse.java
@@ -0,0 +1,13 @@
+package com.pa.inventoryservice.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+}
---
+++ order-service/src/main/java/com/pa/orderservice/config/WebClientConfig.java
@@ -0,0 +1,2 @@
+package com.pa.orderservice.config;public class WebClientConfig {
+}
